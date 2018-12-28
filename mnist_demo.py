81fbc539dbfadcad429002bd4cfa9d6b546fb624
niedu02 <duane.nielsen.rocks@gmail.com>
2018-12-28 14:06:36 -0800

    Added pixel normalization

--- mnist_demo.py
+++ mnist_demo.py
@@ -8,7 +8,7 @@ from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
 from storm.vis import UniImageViewer
 import statistics
-
+from tqdm import tqdm
 
 if __name__ == '__main__':
     in_features = 28 * 28
@@ -16,19 +16,24 @@ if __name__ == '__main__':
     epochs = 20
     batch_size = 200
 
-    rand_net = nn.Sequential(nn.Linear(in_features, h_size), nn.BatchNorm1d(h_size), nn.ReLU(), nn.Linear(h_size, 1), nn.ReLU())
-    dist_net = nn.Sequential(nn.Linear(in_features, h_size), nn.BatchNorm1d(h_size), nn.ReLU(), nn.Linear(h_size, 1), nn.ReLU())
+    rand_net = nn.Sequential(nn.Linear(in_features, h_size), nn.BatchNorm1d(h_size), nn.ReLU(), nn.Linear(h_size, 1),
+                             nn.ReLU())
+    dist_net = nn.Sequential(nn.Linear(in_features, h_size), nn.BatchNorm1d(h_size), nn.ReLU(), nn.Linear(h_size, 1),
+                             nn.ReLU())
 
 
     class MNISTZeroSampler(Sampler):
         """
         Samples only zeros from MNIST
         """
+
         def __init__(self, datasource):
             super().__init__(datasource)
             self.datasource = datasource
             self.zero_elems = []
-            for index in range(len(self.datasource)):
+            len_datasource_tq = tqdm(range(len(self.datasource)))
+            len_datasource_tq.set_description('building index of zeroes')
+            for index in len_datasource_tq:
                 image, label = self.datasource[index]
                 if label.item() == 0:
                     self.zero_elems.append(index)
@@ -44,11 +49,14 @@ if __name__ == '__main__':
         """
         Samples everything but zeros from MNIST
         """
+
         def __init__(self, datasource):
             super().__init__(datasource)
             self.datasource = datasource
             self.zero_elems = []
-            for index in range(len(self.datasource)):
+            len_datasource_tq = tqdm(range(len(self.datasource)))
+            len_datasource_tq.set_description('building index of non zeroes')
+            for index in len_datasource_tq:
                 image, label = self.datasource[index]
                 if label.item() != 0:
                     self.zero_elems.append(index)
@@ -61,22 +69,77 @@ if __name__ == '__main__':
 
 
     mnist = MNIST('mnistdata', download=True,
-                        transform=transforms.Compose([
-                      transforms.ToTensor(),
-                      transforms.Normalize((0.1307,), (0.3081,))
+                  transform=transforms.Compose([
+                      transforms.ToTensor()
                   ]))
-    mnist_zeros = DataLoader(mnist, batch_size=batch_size, sampler=MNISTZeroSampler(mnist))
-    mnist_non_zeros = DataLoader(mnist, batch_size=batch_size, sampler=MNISTNonZeroSampler(mnist))
+
     mnist_normal = DataLoader(mnist, batch_size=batch_size)
 
-    #viewer = UniImageViewer('mnist')
+
+    def compute_covar():
+        sum_image = torch.zeros(in_features).double()
+        sum_squares_dev = torch.zeros(in_features).double()
+        mnist_normal_tq = tqdm(mnist_normal)
+        mnist_normal_tq.set_description('computing covariance matrix - mean')
+        for images, labels in mnist_normal_tq:
+            flat_images = images.squeeze().view(-1, in_features).double()
+            sum_image = sum_image + torch.sum(flat_images, dim=0)
+
+        mean = sum_image / len(mnist)
+
+        mnist_normal_tq = tqdm(mnist_normal)
+        mnist_normal_tq.set_description('computing covariance matrix - stdev')
+        for images, labels in mnist_normal_tq:
+            flat_images = images.squeeze().view(-1, in_features).double()
+            flat_images_dev = flat_images - mean
+            sum_squares_dev = sum_squares_dev + torch.sum(flat_images_dev ** 2, dim=0)
+
+        stdev = torch.sqrt((sum_squares_dev / (len(mnist) - 1)))
+        stdev[stdev == 0.0] = 1e-12
+        return mean, stdev
+
+
+    mean, stdev = compute_covar()
+    mean = mean.view(-1, 28, 28).float()
+    stdev = stdev.view(-1, 28, 28).float()
+
+
+    class Whiten(object):
+        def __init__(self, mean, stdev):
+            self.mean = mean
+            self.stdev = stdev
+
+        def __call__(self, x):
+            return (x - self.mean) / self.stdev
+
+
+    class Clip(object):
+        def __init__(self, clip):
+            self.clip = clip
+
+        def __call__(self, x):
+            return x.clamp(self.clip)
+
+
+    mnist_white = MNIST('mnistdata', download=True,
+                        transform=transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize(mean, stdev)
+                        ]))
+
+    mnist_normal = DataLoader(mnist_white, batch_size=batch_size)
+    mnist_zeros = DataLoader(mnist_white, batch_size=batch_size, sampler=MNISTZeroSampler(mnist))
+    mnist_non_zeros = DataLoader(mnist_white, batch_size=batch_size, sampler=MNISTNonZeroSampler(mnist))
+
+    viewer = UniImageViewer('mnist')
 
     criterion = MSELoss()
-    optimizer = Adam(lr=1e-3, params=dist_net.parameters())
+    optimizer = Adam(lr=1e-4, params=dist_net.parameters())
 
     for epoch in range(epochs):
         train_losses = []
         for image, label in mnist_zeros:
+            # viewer.render(image, block=True)
             flat_image = image.squeeze().view(-1, in_features)
             target = rand_net(flat_image)
             target = target.detach()
@@ -122,12 +185,13 @@ if __name__ == '__main__':
         mean_test_zero_loss = statistics.mean(test_zero_losses)
         mean_test_nonzero_loss = statistics.mean(test_nonzero_losses)
 
+
         def normalize(value, mean, sigma):
-            return (value - mean) / sigma
+            return (value - mean) + 1e-10 / (sigma + 1e-10)
+
 
         normalized_zero = normalize(mean_test_zero_loss, mean_normal_loss, sigma_normal_loss)
         normalized_nonzero = normalize(mean_test_nonzero_loss, mean_normal_loss, sigma_normal_loss)
-
 
         print(f'epoch: {epoch} '
               f'train:{mean_training_loss:.6f} '
--- mnist_novelty_sampling.py
+++ mnist_novelty_sampling.py
@@ -7,38 +7,104 @@ from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
 from torch.nn.functional import softmax
 import statistics
+from tensorboardX import SummaryWriter
+from torch.utils.data.sampler import Sampler
+import random
 
 if __name__ == '__main__':
     in_features = 28 * 28
-    h_size = 20
-    epochs = 20
-    batch_size = 200
-    novel_batch_size = 2
+    h_size = 50
+    epochs = 10
+    batch_size = 1000
+    novel_batch_size = 200
     sample = False
+    magnitude_norm = False
+    L1_distance = True
+    tb = SummaryWriter('runs/biased_2_layer_3')
 
-    rand_net = nn.Sequential(nn.Linear(in_features, h_size), nn.BatchNorm1d(h_size), nn.ReLU(), nn.Linear(h_size, 1),
+    rand_net = nn.Sequential(nn.Linear(in_features, h_size),
+                             nn.BatchNorm1d(h_size),
+                             nn.ReLU(),
+                             nn.Linear(h_size, h_size),
+                             nn.BatchNorm1d(h_size),
+                             nn.ReLU(),
+                             nn.Linear(h_size, 1),
                              nn.ReLU())
-    dist_net = nn.Sequential(nn.Linear(in_features, h_size), nn.BatchNorm1d(h_size), nn.ReLU(), nn.Linear(h_size, 1),
+
+    dist_net = nn.Sequential(nn.Linear(in_features, h_size),
+                             nn.BatchNorm1d(h_size),
+                             nn.ReLU(),
+                             nn.Linear(h_size, h_size),
+                             nn.BatchNorm1d(h_size),
+                             nn.ReLU(),
+                             nn.Linear(h_size, 1),
                              nn.ReLU())
+
+    class MNISTBiasedSampler(Sampler):
+        """
+        Samples only zeros from MNIST
+        """
+
+        def __init__(self, datasource, index, drop_freq):
+            super().__init__(datasource)
+            self.datasource = datasource
+            self.sampled_elems = []
+            for index in range(len(self.datasource)):
+                image, label = self.datasource[index]
+                drop = random.random() < drop_freq
+                if label.item() in {1, 2, 3}:
+                    if not drop:
+                        self.sampled_elems.append(index)
+                else:
+                    self.sampled_elems.append(index)
+
+        def __iter__(self):
+            return iter(self.sampled_elems)
+
+        def __len__(self):
+            return len(self.sampled_elems)
+
 
     mnist = MNIST('mnistdata', download=True,
                   transform=transforms.Compose([
                       transforms.ToTensor(),
                       transforms.Normalize((0.1307,), (0.3081,))
                   ]))
+    mnist_biased = DataLoader(mnist, batch_size=batch_size, sampler=MNISTBiasedSampler(mnist, {1,2,3}, 0.5))
     mnist = DataLoader(mnist, batch_size=batch_size)
 
     criterion = MSELoss()
     optimizer = Adam(lr=1e-3, params=dist_net.parameters())
 
+    base_counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    novelty_counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    global_step = 0
+
     for epoch in range(epochs):
         train_losses = []
-        for image, label in mnist:
-            # compute the novelty value of the image
+        for image, labels in mnist_biased:
             flat_image = image.squeeze().view(-1, in_features)
+
+            for label in labels:
+                base_counts[label.item()] += 1
+            total = sum(base_counts)
+            freq = [x / total for x in base_counts]
+            print(freq)
+
+
+            # normalize by total magnitude of the image
+            if magnitude_norm:
+                ave_sum_image = torch.sum(flat_image, 1) / flat_image.size(1)
+                flat_image = flat_image / ave_sum_image.unsqueeze(1)
+
+            # compute the novelty value of the image
             target = rand_net(flat_image)
             prediction = dist_net(flat_image)
-            error = (prediction - target) ** 2
+            if L1_distance:
+                error = (prediction - target)
+                error[error < 0] = error[error < 0] * -1
+            else:
+                error = (prediction - target) ** 2
             weighted_novelty = softmax(error.squeeze(), dim=0)
 
             if sample:
@@ -48,7 +114,7 @@ if __name__ == '__main__':
                 values, indices = torch.topk(weighted_novelty, novel_batch_size)
 
             novel_images = flat_image[indices]
-            novel_labels = label[indices]
+            novel_labels = labels[indices]
 
             # train distillation network on selected images
             novel_targets = rand_net(novel_images)
@@ -59,6 +125,15 @@ if __name__ == '__main__':
             loss = criterion(prediction, novel_targets)
             loss.backward()
             optimizer.step()
-
             train_losses.append(loss.item())
-            print(novel_labels, statistics.mean(train_losses))
+
+            for label in novel_labels:
+                novelty_counts[label.item()] += 1
+            total = sum(novelty_counts)
+            freq = [x / total for x in novelty_counts]
+            print(freq, statistics.mean(train_losses))
+
+            tb.add_scalar('loss', loss.item(), global_step)
+            tb.add_histogram('labels', labels.cpu().numpy(), epoch)
+            tb.add_histogram('novel_labels', novel_labels.cpu().numpy(), epoch)
+            global_step += 1
